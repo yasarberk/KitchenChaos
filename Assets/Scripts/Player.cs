c96776c6840ca6572932d791a82df9bc33fa994e
Berk Yaşar <93190355+yasarberk@users.noreply.github.com>
2023-03-06 00:33:34 +0300

    v0.1
    -Animations added
    -Cinemachine virtual camera added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,30 +5,27 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
+
+    private bool isWalking;
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y += 1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x -= 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y -= 1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x += 1;
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        //Move direction changes
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDirection * Time.deltaTime * moveSpeed;
+
+        isWalking = moveDirection != Vector3.zero;
+
+        //More smooth rotation
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
