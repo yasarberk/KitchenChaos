19ed72ce4a9b22ed0a836dbba3c8e27af1ccbe6d
Berk Yaşar <93190355+yasarberk@users.noreply.github.com>
2023-03-13 16:39:53 +0300

    v0.3.4
    -Delivery counter added
    -Delivery counter shader added

---
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject())
+        {
+            //Player has a kitchen object
+            if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+            {
+                //Only accepts plates
+                player.GetKitchenObject().DestroySelf();
+            }
+        }
+    }
+}
