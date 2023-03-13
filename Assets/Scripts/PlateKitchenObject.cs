ed79b142bc63cc33a5efd531e607536538f5939b
Berk Yaşar <93190355+yasarberk@users.noreply.github.com>
2023-03-13 15:04:23 +0300

    v0.3.3
    -Complete plate visual
    -Now you can see the ingredients of the plate with icons.

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs 
+    {
+        public KitchenObjectSO kitchenObjectSO;
+    }
+
     [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
     private List<KitchenObjectSO> kitchenObjectSOList;
 
@@ -28,7 +35,18 @@ public class PlateKitchenObject : KitchenObject
         {
             //Add new type
             kitchenObjectSOList.Add(kitchenObjectSO);
-            return true;
+
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs
+            {
+                kitchenObjectSO = kitchenObjectSO
+            });
+
+        return true;
         }
+    }
+
+    public List<KitchenObjectSO> GetKitchenObjectSOList()
+    {
+        return kitchenObjectSOList;
     }
 }
